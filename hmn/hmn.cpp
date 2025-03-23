2425b03d1f210f55a0c5290434f07c1f71b83daf
0x <109202812+exxortq4@users.noreply.github.com>
2025-03-23 10:18:20 +0300

    Add files via upload

---
+++ hmn/hmn.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <curl/curl.h>
+#include <string>
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+int main() {
+
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    CURL* curl;
+    CURLcode res;
+    std::string readBuffer;
+
+    curl = curl_easy_init();
+    if (curl) {
+        std::string email;
+        std::cout << "Enter your email: ";
+        std::cin >> email;
+
+        std::string postData = "demo_mail=" + email;
+
+        curl_easy_setopt(curl, CURLOPT_URL, "https://hidxmy.name/demo/success/");
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postData.c_str());
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            std::cerr << "cURL error: " << curl_easy_strerror(res) << std::endl;
+        }
+        else {
+            std::cout << "Response: " << readBuffer << std::endl;
+        }
+
+        curl_easy_cleanup(curl);
+    }
+    return 0;
+}
